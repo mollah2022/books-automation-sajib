f1fccff04999dc851f77c50fd02f37894b871158
mollah2022 <sajibahmed12032022@gmail.com>
2026-06-28 13:54:19 +0600

    feat: add BookDetailPage page object with title, price and product info methods

---
+++ pages/book_detail_page.py
@@ -0,0 +1,31 @@
+from playwright.sync_api import Page
+from pages.base_page import BasePage
+
+
+class BookDetailPage(BasePage):
+    """Page object for individual book detail page."""
+
+    _H1_TITLE = "article.product_page h1"
+    _PRICE = "p.price_color"
+    _PRODUCT_INFO = "table.table-striped"
+    _PRODUCT_GALLERY = "div.item.active img"
+
+    def __init__(self, page: Page) -> None:
+        super().__init__(page)
+
+    def get_title(self) -> str:
+        self.wait_for_selector(self._H1_TITLE)
+        return self.get_text(self._H1_TITLE)
+
+    def get_price(self) -> str:
+        self.wait_for_selector(self._PRICE)
+        return self.get_text(self._PRICE)
+
+    def is_product_info_visible(self) -> bool:
+        return self.is_visible(self._PRODUCT_INFO)
+
+    def is_image_visible(self) -> bool:
+        return self.is_visible(self._PRODUCT_GALLERY)
+
+    def go_to_homepage(self) -> None:
+        self.go_back()
